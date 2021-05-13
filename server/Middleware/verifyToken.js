38e2bdfbb48199f9d6eaa5a57257e63ecb4490e5
Viet Hung <viethung01234@gmail.com>
2021-05-13 10:33:17 +0700

    add Verify Token Middleware

--- server/Middleware/verifyToken.js
+++ server/Middleware/verifyToken.js
@@ -2,12 +2,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
   const token = req.header("auth-token");
-  console.log(typeof token);
+
   if (!token) return res.status(401).send("Access denied");
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user(verified);
+    req.user = verified;
     next();
   } catch (error) {
     res.status(400).send("invalid token");
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../Middleware/verifyToken");
 
 const userController = require("../app/controllers/UserController");
 
 router.get("/list", userController.list);
-router.get("/test", verifyToken, userController.test);
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
