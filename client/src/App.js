caff4890628f16e9233853c1a26d96eef2c22912
Viet Hung <viethung01234@gmail.com>
2021-05-27 11:38:12 +0700

    [UPDATE]: Remove Admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import Cart from './components/Cart/Cart';
 import Navbar from './components/Navbar/Navbar';
-import Admin from './pages/Admin';
 import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,7 +15,7 @@ import { getCart } from './actions/cartActions';
 
 function App() {
   const dispatch = useDispatch();
-  const cartLength = useSelector(state => state.cart.cartItems.length);
+  const cart = useSelector(state => state.cart.cartItems);
   const user = useSelector(state => state.user.user);
 
   useEffect(() => {
@@ -26,11 +26,10 @@ function App() {
 
   return (
     <ChakraProvider theme={theme}>
-      <Navbar cart={cartLength} />
+      <Navbar cart={cart} />
 
       <Switch>
         <Route exact path="/" component={Home}></Route>
-        <Route path="/admin" component={Admin}></Route>
         <Route path="/cart" render={props => <Cart {...props} />} />
 
         <Route path="/login" component={Login}></Route>
