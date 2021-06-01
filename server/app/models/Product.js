ef0441e70a21825f5359dc4175ace7519aad2a01
Viet Hung <viethung01234@gmail.com>
2021-06-01 19:44:58 +0700

    [Update]: ProductModel Update: slug, desc, discount, categories

--- server/app/models/Product.js
+++ server/app/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-generator");
 
 const Schema = mongoose.Schema;
 
@@ -8,9 +9,19 @@ const ProductSchema = new Schema(
       type: String,
       required: true,
     },
+    slug: {
+      type: String,
+      unique: true,
+      slug: "name",
+    },
     price: {
       type: Number,
       required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+      default: "",
     },
     quantity: {
       type: Number,
@@ -20,11 +31,24 @@ const ProductSchema = new Schema(
       type: String,
       required: true,
     },
+    discount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    categories: {
+      type: Array,
+      required: true,
+      default: [],
+    },
   },
   {
     timestamps: true,
   }
 );
+
+// Add plugin
+mongoose.plugin(slug);
 
 const Product = mongoose.model("Product", ProductSchema);
 module.exports = Product;
