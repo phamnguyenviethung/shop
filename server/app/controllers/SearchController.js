369114b920e28e3098f7fba33139b69c6d9c4861
Viet Hung <viethung01234@gmail.com>
2021-06-18 11:18:45 +0700

    [ADD]: CRUD Factory, API Features: Filter, Sort, Limit Fields, Pagination, Error handler

--- server/app/controllers/SearchController.js
+++ server/app/controllers/SearchController.js
@@ -1,19 +1,48 @@
 const ProductModel = require("../models/Product");
+const isEmpty = require("../../util/isObjEmpty");
 
 class SearchController {
   search(req, res) {
-    const { name } = req.query;
+    // if (isEmpty(req.query)) {
+    //   ProductModel.find({})
+    //     .then((rs) => res.send(rs))
+    //     .catch((err) => console.log(err));
+    // }
 
-    ProductModel.find({})
-      .then((result) => {
-        const filter = [];
+    // Query
+    const { name, price, size, color } = req.query;
 
-        result.map((item) => {
-          if (item.name.toLowerCase().includes(name.toLowerCase())) {
-            filter.push(item);
-          }
+    //  Query convert
+    const text = name
+      ? name
+          .trim()
+          .toLowerCase()
+          .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))
+      : "";
+
+    const priceRange = price ? price.map((i) => i * 100) : [0, 9e9];
+
+    // Query assignment
+
+    const query = {};
+    const queryAssign = (q, key = q, value = q) => {
+      if (q) {
+        Object.assign(query, {
+          [key]: value,
         });
-        res.send(filter);
+      }
+    };
+    queryAssign(price, "price", { $gt: priceRange[0], $lt: priceRange[1] });
+    queryAssign(size, "size", { $in: size });
+    queryAssign(color, "color", { $in: color });
+
+    // Find
+
+    ProductModel.find({
+      $and: [{ ...query }, { name: { $regex: text } }],
+    })
+      .then((rs) => {
+        res.send(rs);
       })
       .catch((err) => res.send(err));
   }
--- server/index.js
+++ server/index.js
@@ -5,14 +5,26 @@ const app = express();
 const db = require("./config/db");
 const route = require("./routes");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const AppError = require("./util/appError");
+const globalErrorHandler = require("./app/controllers/errorController");
+
 dotenv.config();
 // Connect to DB
 db.connect();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("combined"));
 
 route(app);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 const PORT = 3001;
 app.listen(PORT, () => {
