5d02d5bea2b3568231a966f0eb5e21a52a923d5a
Viet Hung <viethung01234@gmail.com>
2021-07-18 20:15:39 +0700

    [ADD]: Get Public Info User

--- server/app/controllers/UserController.js
+++ server/app/controllers/UserController.js
@@ -1,4 +1,22 @@
 const User = require("../models/User");
 const factory = require("./handlerFactory");
+const catchAsync = require("../../utils/catchAsync");
 
 exports.getAll = factory.getAll(User);
+exports.getOne = catchAsync(async (req, res, next) => {
+  let doc = await User.findOne({ uid: req.user.uid });
+
+  if (!doc) {
+    return next(new AppError("No document found with that ID", 404));
+  }
+
+  // Hide private data
+  delete doc.password;
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: doc,
+    },
+  });
+});
